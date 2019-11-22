1b295d3e59efe5e1b00f719860eaa48a448f1837
Zina Ramirez <instinctualart@gmail.com>
2019-11-21 20:16:03 -0500

    Initial template

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include('include/header.php'); ?>
+<main>
+  <nav id="topnav">
+    <a href="#" class="cursive brand">Artemisia Gentileschi</a>
+  </nav>
+
+  <div class="pure-g flex-1">
+    <div class="pure-u-4-5">
+      <div class="p-2">
+      <h1>Hello World</h1>
+      <p>foobar</p>
+      </div>
+    </div>
+    <div id="sidenav" class="pure-u-1-5 flex-1">
+      Aisde
+    </div>
+    
+  </div>
+</main>
+<?php include('include/footer.php'); ?>
